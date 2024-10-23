5a5f166a500d8683274523905fd4ee574677bd72
Javier02R <rodriguezjavier11g@gmail.com>
2024-10-23 01:45:25 -0600

    animaciones+botones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations'; // Importa las herramientas de animación
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('400ms ease-out', style({ transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
 export class HomePage {
 
--- src/app/tourist-site-list/tourist-site-list.page.ts
+++ src/app/tourist-site-list/tourist-site-list.page.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations'; 
 
 @Component({
   selector: 'app-tourist-site-list',
   templateUrl: './tourist-site-list.page.html',
   styleUrls: ['./tourist-site-list.page.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class TouristSiteListPage implements OnInit {
 
